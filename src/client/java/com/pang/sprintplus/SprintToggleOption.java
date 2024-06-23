76c6048946445e7a5418b60178202bddefc53a76
Hol1ow <110432668+Hol1ow@users.noreply.github.com>
2024-06-22 22:14:57 -0400

    new toggle option

--- src/client/java/com/pang/sprintplus/SprintToggleOption.java
+++ src/client/java/com/pang/sprintplus/SprintToggleOption.java
@@ -3,7 +3,7 @@ package com.pang.sprintplus;
 import net.minecraft.client.option.SimpleOption;
 import net.minecraft.text.Text;
 
-public class SprintToggleOption {
+public abstract class SprintToggleOption {
 
     private final static Text TOGGLE_KEY_TEXT = Text.translatable("options.key.toggle");
     private final static Text HOLD_KEY_TEXT = Text.translatable("options.key.hold");
