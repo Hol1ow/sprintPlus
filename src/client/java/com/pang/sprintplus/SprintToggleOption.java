c146a23df3b868c0cf002de8e5696b3539301866
Hol1ow <110432668+Hol1ow@users.noreply.github.com>
2024-06-22 22:13:49 -0400

    adding extra game option for mode

---
+++ src/client/java/com/pang/sprintplus/SprintToggleOption.java
@@ -0,0 +1,17 @@
+package com.pang.sprintplus;
+
+import net.minecraft.client.option.SimpleOption;
+import net.minecraft.text.Text;
+
+public class SprintToggleOption {
+
+    private final static Text TOGGLE_KEY_TEXT = Text.translatable("options.key.toggle");
+    private final static Text HOLD_KEY_TEXT = Text.translatable("options.key.hold");
+
+    public final static SimpleOption<Boolean> sprintPlusToggle = new SimpleOption<Boolean>(
+            "key.sprintPlus.sprintPlusKey", SimpleOption.emptyTooltip(),
+            (optionText, value) -> {
+                return value ? TOGGLE_KEY_TEXT : HOLD_KEY_TEXT;
+            }, SimpleOption.BOOLEAN, true, (value) -> {
+            });
+}
